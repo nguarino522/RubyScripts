70b1f4bc4c358b4a29f500cdca2ecde2f4226f8f
Nicholas Guarino <nguarino@datto.lan>
2018-07-19 13:30:00 -0400

    first working process

--- bfy_grab_gdrive_gmail_service_ids.rb
+++ bfy_grab_gdrive_gmail_service_ids.rb
@@ -5,18 +5,28 @@ email_array = [ARRAY_OF_EMAILS_PROVIDED]
 #Set the customer id
 c = Customer.find [CUSTOMER_ID]
 
-#Declare service id array i.e. input
+#Declare necessary arrays, final input array and array of parent ids
 input = Array.new
+parent_id_array = Array.new
 
-
-#Pull each parent service ID for each email and
-#pull each service for gdrive and gmail from parent service ID and send to input array
+#Pull each parent service ID and output to parent service id array
 email_array.each do |emails|
     user = User.find_by_email emails
     parent_service = user.services
-    parent_service_id = parent_service.ids
-    service = c.services.where(:type => "GoogleDocsService", :parent_service_id => parent_service_id)
-    input.push(service.ids.map(&:to_s))
-    service = c.services.where(:type => "GoogleMailService", :parent_service_id => parent_service_id)
-    input.push(service.ids.map(&:to_s))
+    parent_service_id = parent_service.ids.join
+    parent_id_array.push(parent_service_id)
+end
+
+#populate input array with gmail service ids
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleDocsService", :parent_service_id => id).where.not(:disabled_code => 13)
+    service_id = service.ids.join
+    input.push(service_id)
+end
+
+#populate input array with gdocs service ids
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleMailService", :parent_service_id => id).where.not(:disabled_code => 13)
+    service_id = service.ids.join
+    input.push(service_id)
 end
