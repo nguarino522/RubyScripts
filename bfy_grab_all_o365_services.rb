b3f4d7a72653d55b729b907f6f45e67daef449ee
Nicholas Guarino <nguarino@datto.lan>
2018-10-23 13:49:52 -0400

    first commit

---
+++ bfy_grab_all_o365_services.rb
@@ -0,0 +1,14 @@
+
+email_array = [ARRAY_OF_EMAILS_PROVIDED]
+
+c = Customer.find [CUSTOMER_ID]
+
+input = Array.new
+services = Array.new
+
+email_array.each do |emails|
+    seat = Seat.find_by_principal_name emails
+    services = seat.services
+    services.push(seat.services)
+end
+
