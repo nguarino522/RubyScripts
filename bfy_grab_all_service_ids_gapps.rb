a0677ce55409e6d5792871cb32454fceba456272
Nicholas Guarino <nguarino@datto.lan>
2018-11-14 12:39:48 -0500

    first commit

---
+++ bfy_grab_all_service_ids_gapps.rb
@@ -0,0 +1,41 @@
+#Create and grab a collection of emails you want to grab service ids for
+email_array = [ARRAY_OF_EMAILS_PROVIDED]
+
+#Set the customer id
+c = Customer.find [CUSTOMER_ID]
+
+#Declare necessary arrays
+services = Array.new
+parent_id_array = Array.new
+
+email_array.each do |emails|
+    user = User.find_by_email emails
+    parent_service = user.services
+    parent_service_id = parent_service.ids.join
+    parent_id_array.push(parent_service_id)
+    services.push(parent_service_id)
+end
+
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleDocsService", :parent_service_id => id)
+    service_id = service.ids.join
+    services.push(service_id)
+end
+
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleMailService", :parent_service_id => id)
+    service_id = service.ids.join
+    services.push(service_id)
+end
+
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleCalendarService", :parent_service_id => id)
+    service_id = service.ids.join
+    services.push(service_id)
+end
+
+parent_id_array.each do |id|
+    service = c.services.where(:type => "GoogleContactsService", :parent_service_id => id)
+    service_id = service.ids.join
+    services.push(service_id)
+end
