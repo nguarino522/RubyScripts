30654e98be8a4492ea2ff13f9770bd1f672cc46c
nguarino522 <nguarino@NICHOLAS-PC.localdomain>
2022-06-27 23:47:34 -0400

    update checksum calculation complete

--- convert_md5_log_changes.rb
+++ convert_md5_log_changes.rb
@@ -3,18 +3,11 @@
 require 'digest'
 
 script_file_command = `/home/nguarino/list_username_homedirectory.rb`
-usernames = script_file_command.split
+usernames = script_file_command
+hash = Digest::MD5.hexdigest(usernames)
 
-usernames.each do |u|
-	puts Digest::MD5.hexdigest(u)
-end
-
-#if(File.file?('/var/log/current_users'))
-#	puts "exists"
+unless File.file?('/var/log/current_users')
+       
 #elsif(File.
 
-#end
 
-#File.open("/var/log/current_users", "mode"
-
-puts Digest::MD5.hexdigest("nguarino:/home/nguarino")
