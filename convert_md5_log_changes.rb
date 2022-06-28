9c6b7585872e89bfdd6e2b48780a5e2899a236ad
Nicholas Guarino <nguarino@nguarino-test.datto.lan>
2022-06-28 16:10:54 +0000

    completed both scripts

--- convert_md5_log_changes.rb
+++ convert_md5_log_changes.rb
@@ -6,8 +6,22 @@ script_file_command = `/home/nguarino/list_username_homedirectory.rb`
 usernames = script_file_command
 hash = Digest::MD5.hexdigest(usernames)
 
-unless File.file?('/var/log/current_users')
-       
-#elsif(File.
-
-
+if not File.exists?("/var/log/current_users")
+	File.open("/var/log/current_users", "w") do |f|
+		f << hash
+	end
+else 
+	old_hash = `cat /var/log/current_users`
+	if old_hash != hash
+		File.open("/var/log/current_users", "w") do |f|
+			f << hash
+		end
+		File.open("/var/log/user_changes", "a") do |f|
+			date = `date`
+			date.delete!("\n")
+			f << "#{date} changes occurred"
+		end
+	else
+		exit
+	end
+end
